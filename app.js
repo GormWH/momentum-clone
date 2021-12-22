76b6c9488c2195a597d423ba689a947c42feb3a2
suhong_park <sp4kid@gmail.com>
2021-12-22 23:46:12 +0900

    before #5

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function sayHello() {
-    console.log("Hello");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+    console.log(userName);
 }
 
-sayHello();
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
